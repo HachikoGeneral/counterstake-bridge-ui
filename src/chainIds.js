17d2a6d1eabb5bf4f518377b86869a2f1d4ff5a8
HachikoGeneral <93988533+HachikoGeneral@users.noreply.github.com>
2022-12-16 20:06:22 +0000

    Update chainIds.js

--- src/chainIds.js
+++ src/chainIds.js
@@ -1,16 +1,16 @@
 export const chainIds = {
   mainnet: {
     Ethereum: 1,
-    BSC: 56,
+    Chikochain: 42,
     Polygon: 137
   },
   testnet: {
     Ethereum: 4, // rinkeby
-    BSC: 97,
+    Chikochain: 42,
     Polygon: 80001
   },
   devnet: {
     Ethereum: 1337, // ganache
-    BSC: null,
+    Chikochain: null,
   },
 };
